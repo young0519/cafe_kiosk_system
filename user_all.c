3972ac1e1850462f1276cd2497853c1db209fc25
DESKTOP-ANMPDE6\ESTEL <kiny0606@naver.com>
2021-08-22 23:09:37 +0900

    Add a detailed description of the code

--- user_all.c
+++ user_all.c
@@ -1,15 +1,16 @@
 #include<stdio.h>
 #include <string.h>
 #include <memory.h>
-
+#include <stdlib.h>
+// 전역 변수 설정
+FILE* user;
 
 int main(void) {
 	// 변수 설정
-	int member_menu;
-	FILE* user;
-	char name[20];
-	char phonenumber[30];
-	int point = 0;
+	int member_menu;		// member_menu: 회원 기능 선택 변수
+	char name[20];			// name: 회원 이름 
+	char phonenumber[30];	// phonenumber: 회원 전화번호
+	int point = 0;			// point 회원의 현재 포인트
 
 
 	printf("-------------------------------------------\n");
@@ -20,7 +21,9 @@ int main(void) {
 	printf("\n1. 회원 가입\t2. 회원 정보 확인\n");
 	scanf("%d", &member_menu);
 	printf("-------------------------------------------\n");
-	if (member_menu == 1) {			// 회원 가입 
+
+	// 회원 가입
+	if (member_menu == 1) {
 		printf("\t고객님 안녕하세요.\n");
 		printf("\t회원 가입 화면입니다.\n");
 		printf("-------------------------------------------\n");
@@ -36,7 +39,7 @@ int main(void) {
 			printf("고객님께서 입력하신 정보입니다.\n");
 			printf("이름: %s  전화번호:  %s\n", name, phonenumber);
 			printf("맞으면 1, 틀리면 2를 입력해주세요\n");
-			printf("★★1과 2가 아닌 번호를 입력하게 되면 정보를 다시 입력해야합니다★★\n");
+			printf("★★1과 2가 아닌 번호를 입력하게 되면 정보를 다시 입력해야 합니다★★\n");
 			printf("입력:");
 			scanf("%d", &info_num);
 			if (info_num == 1) {
