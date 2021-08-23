4ba9a9bad8ed279f5693f0ad314210669e082bab
DESKTOP-ANMPDE6\ESTEL <kiny0606@naver.com>
2021-08-23 21:16:32 +0900

    Add a member join code to all member-related function
    The membership part of the main function has been separated into functions.

--- user_all.c
+++ user_all.c
@@ -10,7 +10,42 @@ char name[20];			// name: 회원 이름
 char phonenumber[30];	// phonenumber: 회원 전화번호
 int point = 0;			// point 회원의 현재 포인트
 
-//회원찾는 함수
+// 회원 가입하는 함수
+void user_join() {
+	printf("\t고객님 안녕하세요.\n");
+	printf("\t회원 가입 화면입니다.\n");
+	printf("-------------------------------------------\n");
+	user = fopen("user_info.txt", "a");
+	if (user == NULL) {
+		printf("파일 열기 실패\n");
+	}
+	while (1) {
+		int info_num;
+		printf("고객님의 정보를 입력해주세요(양식: 홍길동 01012345678): ");
+		scanf("%s %s", &name, &phonenumber);
+		printf("------------------로딩중-------------------\n");
+		printf("고객님께서 입력하신 정보입니다.\n");
+		printf("이름: %s  전화번호:  %s\n", name, phonenumber);
+		printf("맞으면 1, 틀리면 2를 입력해주세요\n");
+		printf("★★1과 2가 아닌 번호를 입력하게 되면 정보를 다시 입력해야 합니다★★\n");
+		printf("입력:");
+		scanf("%d", &info_num);
+		if (info_num == 1) {
+			break;
+		}
+		else
+			continue;
+	}
+	printf("-------------------로딩중------------------\n");
+	fprintf(user, "%s %s    %d\n", name, phonenumber, point);
+	fclose(user);
+	printf("-------------------------------------------\n");
+	printf("%s님 회원가입이 완료됐습니다.\n", name);
+	printf("많은 이용 바랍니다. 감사합니다.\n");
+	printf("-------------------------------------------\n");
+}
+
+//회원 찾는 함수
 void user_search() {
 	char user_get[100], user_ans[100];
 	int num = 0, pass = 0;
@@ -71,51 +106,37 @@ int main(void) {
 	printf("-------------------------------------------\n");
 	printf("이용하고자 하는 서비스의 번호를 입력해주세요.\n");
 	printf("\n1. 회원 가입\t2. 회원 정보 확인\n");
+	printf("★★1과 2가 아닌 번호를 입력하게 되면 프로그램이 종료됩니다.★★\n");
+	printf("입력: ");
 	scanf("%d", &member_menu);
 	printf("-------------------------------------------\n");
 
-	// 회원 가입
-	if (member_menu == 1) {
-		printf("\t고객님 안녕하세요.\n");
-		printf("\t회원 가입 화면입니다.\n");
-		printf("-------------------------------------------\n");
-		user = fopen("user_info.txt", "a");
-		if (user == NULL) {
-			printf("파일 열기 실패\n");
+	while (1) {
+		// 회원 가입
+		if (member_menu == 1) {
+			user_join();
+			printf("\t이전 화면으로 돌아갑니다.\n");
+			printf("-------------------------------------------\n");
+			continue;
 		}
-		while (1) {
-			int info_num;
-			printf("고객님의 정보를 입력해주세요(양식: 홍길동 01012345678): ");
-			scanf("%s %s", &name, &phonenumber);
-			printf("------------------로딩중-------------------\n");
-			printf("고객님께서 입력하신 정보입니다.\n");
-			printf("이름: %s  전화번호:  %s\n", name, phonenumber);
-			printf("맞으면 1, 틀리면 2를 입력해주세요\n");
-			printf("★★1과 2가 아닌 번호를 입력하게 되면 정보를 다시 입력해야 합니다★★\n");
-			printf("입력:");
-			scanf("%d", &info_num);
-			if (info_num == 1) {
-				break;
-			}
-			else
-				continue;
+		// 회원 정보 찾기
+		else if (member_menu == 2) {
+			printf("\t고객님 안녕하세요.\n");
+			printf("\t회원 정보 확인 화면입니다.\n");
+			printf("-------------------------------------------\n");
+			user_search();
+			printf("-------------------------------------------\n");
+			user_set();
+			printf("-------------------------------------------\n");
+			printf("\t이전 화면으로 돌아갑니다.\n");
+			printf("-------------------------------------------\n");
+			continue;
 		}
-		printf("-------------------로딩중------------------\n");
-		fprintf(user, "%s %s    %d\n", name, phonenumber, point);
-		fclose(user);
-		printf("-------------------------------------------\n");
-		printf("%s님 회원가입이 완료됐습니다.\n", name);
-		printf("많은 이용 바랍니다. 감사합니다.\n");
-		printf("-------------------------------------------\n");
+		else {
+			printf("프로그램을 이용해주셔서 감사합니다.\n");
+			printf("-------------------------------------------\n");
+			break;
+		}
+	}
 
-	}
-	else if (member_menu == 2) {
-		printf("\t고객님 안녕하세요.\n");
-		printf("\t회원 정보 확인 화면입니다.\n");
-		printf("-------------------------------------------\n");
-		user_search();
-		printf("-------------------------------------------\n");
-		user_set();
-		printf("-------------------------------------------\n");
-	}
 }
